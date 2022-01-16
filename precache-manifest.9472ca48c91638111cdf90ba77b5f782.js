6af3e8b2dbf9714fd98ab97e5335a4536cf8ad6a
johnsuharjono <john.suharjono10@gmail.com>
2022-01-17 00:29:54 +0800

    Updates

---
+++ precache-manifest.9472ca48c91638111cdf90ba77b5f782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa48a4dbbb55d6af73d6698f0fbe656f",
+    "url": "/real-time-mask-detection/index.html"
+  },
+  {
+    "revision": "9a857f97b55d9748253c",
+    "url": "/real-time-mask-detection/static/css/main.0233b93a.chunk.css"
+  },
+  {
+    "revision": "92edf06bf16248bb2564",
+    "url": "/real-time-mask-detection/static/js/2.83bc3ecb.chunk.js"
+  },
+  {
+    "revision": "927d15cad02b8fa4f913e8e01503c169",
+    "url": "/real-time-mask-detection/static/js/2.83bc3ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a857f97b55d9748253c",
+    "url": "/real-time-mask-detection/static/js/main.a2ad6dd7.chunk.js"
+  },
+  {
+    "revision": "a2056d762e618dc9d405",
+    "url": "/real-time-mask-detection/static/js/runtime-main.35d678b9.js"
+  }
+]);
